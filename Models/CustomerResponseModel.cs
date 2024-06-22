43a27b6bb1bffd69cacdd796d7ad2d608c95c1c0
ahthawm <santawmmaran@acedatasystems.com>
2024-06-23 00:49:45 +0630

    http hte mvc shaka ai

---
+++ Models/CustomerResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Webmvc.Models
+{
+    public class CustomerResponseModel
+    {
+        public List<CustomerModel> Data { get; set; }
+
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+    }
+}
