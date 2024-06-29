c151556d6cca46e6e47248902858123ffb507dd5
ahthawm <santawmmaran@acedatasystems.com>
2024-06-29 18:56:55 +0630

    Blazor Server ngut sai re!

--- Models/CustomerModel.cs
+++ Models/CustomerModel.cs
@@ -10,7 +10,7 @@ public class CustomerModel
     public int CustomerId { get; set; }
     public string CustomerName { get; set; }
     public string PhoneNo { get; set; }
-    public DateTime DateOfBirth { get; set; }
+    public DateOnly DateOfBirth { get; set; }
     public string Gender { get; set; }
 }
 }
